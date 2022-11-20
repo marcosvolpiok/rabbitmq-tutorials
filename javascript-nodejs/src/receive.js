b5423f137940c49e68be5c7aa61b54e150a54bde
Marcos Volpi <marcos@marcosvolpi.com>
2022-11-20 11:33:03 -0300

    Test

--- javascript-nodejs/src/receive.js
+++ javascript-nodejs/src/receive.js
@@ -2,7 +2,7 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+amqp.connect('amqp://172.17.0.2', function(error0, connection) {
     if (error0) {
         throw error0;
     }
